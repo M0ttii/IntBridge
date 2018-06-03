e8601378a0376de60aeeb04a5bfe54b5122e9bad
M0ttii <adrian.doernberg@berlin.de>
2018-06-03 13:23:40 +0200

    Replaced Placeholders

--- src/main/java/com/github/m0ttii/intbridge/bungeecord/listener/PubSubListener.java
+++ src/main/java/com/github/m0ttii/intbridge/bungeecord/listener/PubSubListener.java
@@ -1,5 +1,6 @@
 package com.github.m0ttii.intbridge.bungeecord.listener;
 
+import com.github.m0ttii.intbridge.bungeecord.IntBridge;
 import com.github.m0ttii.intbridge.bungeecord.manager.ViolationManager;
 import net.md_5.bungee.api.ProxyServer;
 import redis.clients.jedis.Jedis;
@@ -11,11 +12,11 @@ import java.util.stream.Stream;
 /**
  * Created by Adrian D. on 01.06.2018.
  */
-public class PubSubListener
-{
+public class PubSubListener {
+    Jedis jSubscriber;
     public PubSubListener()
     {
-        Jedis jSubscriber = new Jedis();
+        this.jSubscriber = IntBridge.getInstance().getJedis();
         jSubscriber.subscribe(new JedisPubSub() {
             @Override
             public void onMessage(String channel, String message) {
@@ -25,9 +26,12 @@ public class PubSubListener
                         .getCommands(ViolationManager.getViolation(UUID.fromString(uuid)), ViolationManager.getViolation(UUID.fromString(uuid)) + violation);
 
                 command.forEachOrdered(s ->
-                    ProxyServer.getInstance().getPluginManager().dispatchCommand(ProxyServer.getInstance().getConsole(), s)
+                    ProxyServer.getInstance().getPluginManager().dispatchCommand(ProxyServer.getInstance().getConsole(), s
+                            .replace("[player]", ProxyServer.getInstance().getPlayer(UUID.fromString(uuid)).getName())
+                            .replace("[server]", ProxyServer.getInstance().getPlayer(UUID.fromString(uuid)).getServer().getInfo().getName())
+                            .replace("[violation]", violation.toString()))
                 );
-                ViolationManager.addViolation(UUID.fromString(uuid), ViolationManager.getViolation(UUID.fromString(uuid)) + violation);
+                ViolationManager.addViolation(UUID.fromString(uuid), violation);
 
             }
         }, "intave-violation");
